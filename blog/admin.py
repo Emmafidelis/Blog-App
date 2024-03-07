e4bb843944a3c6309cece81c376748c57306ff79
Emanuel Fidelis <emanuelkagombora28@gmail.com>
2024-03-07 12:51:17 +0300

    Database and migrations

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ['title', 'date_posted', 'author']
 
 # Register your models here.
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-  {
-    'author': 'antony',
-    'title': 'blog post 1',
-    'content': 'first post',
-    'date_posted': 'August 27, 2018'
-  },
-  {
-    'author': 'juma',
-    'title': 'blog post 2',
-    'content': 'second post',
-    'date_posted': 'August 28, 2018'
-  }
-]
 
 def home(request):
+  posts = Post.objects.all()
   context = {
     'posts': posts
   }
